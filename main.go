e82724d5a7bf3e549ca1cf5305a413d45e53f7b4
Panupong Rong-O <tayalone@gmail.com>
2022-11-28 20:18:50 +0700

    🚧 creating gin with prometheus

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
